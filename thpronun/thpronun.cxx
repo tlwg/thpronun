09473a0ed9ebe6bc8c35d4fa33bf143ed3b85198
Theppitak Karoonboonyanan <theppitak@gmail.com>
2018-05-21 11:47:36 +0700

    Add '-g' CLI option for grouped JSON output
    * thpronun/thpronun.cxx:
      - [MakeThaiOutput(), MakeRomanOutput(), MakePhoneticOutput()]
        Add 'isGroup' arg and create GroupedJsonOutput if it's true
      - [main()] Parse '-g' command-line arg and pass its existence
        to Make*Output() functions
      - [Usage()] Add help message for the '-g' option
      - [DoParse()] Call IOutput::output() with the string list arg directly
        instead of iterating the list, so that the grouped JSON output
        becomes effective.

--- thpronun/thpronun.cxx
+++ thpronun/thpronun.cxx
@@ -5,6 +5,7 @@
 #include "output/output-delim.h"
 #include "output/output-roman.h"
 #include "output/output-json.h"
+#include "output/output-gjson.h"
 
 #include <iostream>
 #include <list>
@@ -17,16 +18,8 @@ DoParse (string word, const list<unique_ptr<IOutput>>& stringOutputs)
 {
     cout << word << ":" << endl;
     auto sylList = ParseWord (word);
-    for (const auto& s : sylList) {
-        bool isFirst = true;
-        for (const auto& sylStrOut : stringOutputs) {
-            if (!isFirst) {
-                cout << '\t';
-            }
-            isFirst = false;
-            cout << sylStrOut->output (s);
-        }
-        cout << endl;
+    for (const auto& sylStrOut : stringOutputs) {
+        cout << sylStrOut->output (sylList) << endl;;
     }
 }
 
@@ -36,6 +29,7 @@ Usage (const char* progName)
     cerr << "Usage: " << progName << " [options] [word...]" << endl
          << "Options:" << endl
          << "    -j    Turns on JSON output" << endl
+         << "    -g    Turns on grouping in JSON output (implies '-j')" << endl
          << "    -r    Outputs Romanization" << endl
          << "    -t    Outputs Thai pronunciation" << endl
          << "    -p    Outputs Phonetic form" << endl
@@ -45,34 +39,48 @@ Usage (const char* progName)
 }
 
 static unique_ptr<IOutput>
-MakeThaiOutput (bool isJson)
+MakeThaiOutput (bool isJson, bool isGroup)
 {
     auto thaiSylOutput = make_unique<ThaiSylOutput>();
 
     if (isJson) {
-        return make_unique<JsonOutput> (move (thaiSylOutput));
+        if (isGroup) {
+            return make_unique<GroupedJsonOutput> (move (thaiSylOutput));
+        } else {
+            return make_unique<JsonOutput> (move (thaiSylOutput));
+        }
     } else {
         return make_unique<DelimOutput> (move (thaiSylOutput), '-');
     }
 }
 
 static unique_ptr<IOutput>
-MakeRomanOutput (bool isJson)
+MakeRomanOutput (bool isJson, bool isGroup)
 {
     if (isJson) {
-        return make_unique<JsonOutput> (make_unique<RomanSylOutput>());
+        auto romanSylOutput = make_unique<RomanSylOutput>();
+
+        if (isGroup) {
+            return make_unique<GroupedJsonOutput> (move (romanSylOutput));
+        } else {
+            return make_unique<JsonOutput> (move (romanSylOutput));
+        }
     } else {
         return make_unique<RomanOutput>();
     }
 }
 
 static unique_ptr<IOutput>
-MakePhoneticOutput (bool isJson)
+MakePhoneticOutput (bool isJson, bool isGroup)
 {
     auto phoneticSylOutput = make_unique<PhoneticSylOutput>();
 
     if (isJson) {
-        return make_unique<JsonOutput> (move (phoneticSylOutput));
+        if (isGroup) {
+            return make_unique<GroupedJsonOutput> (move (phoneticSylOutput));
+        } else {
+            return make_unique<JsonOutput> (move (phoneticSylOutput));
+        }
     } else {
         return make_unique<DelimOutput> (move (phoneticSylOutput), ' ');
     }
@@ -83,6 +91,7 @@ main (int argc, const char* argv[])
 {
     int optCnt = 0;
     bool isJson = false;
+    bool isGroup = false;
     list<unique_ptr<IOutput>> stringOutputs;
 
     for (int i = 1; i < argc; ++i) {
@@ -91,14 +100,18 @@ main (int argc, const char* argv[])
             case 'j':
                 isJson = true;
                 break;
+            case 'g':
+                isJson = true;
+                isGroup = true;
+                break;
             case 'r':
-                stringOutputs.push_back (MakeRomanOutput (isJson));
+                stringOutputs.push_back (MakeRomanOutput (isJson, isGroup));
                 break;
             case 't':
-                stringOutputs.push_back (MakeThaiOutput (isJson));
+                stringOutputs.push_back (MakeThaiOutput (isJson, isGroup));
                 break;
             case 'p':
-                stringOutputs.push_back (MakePhoneticOutput (isJson));
+                stringOutputs.push_back (MakePhoneticOutput (isJson, isGroup));
                 break;
             case 'h':
                 Usage (argv[0]);
@@ -112,9 +125,9 @@ main (int argc, const char* argv[])
         }
     }
     if (stringOutputs.empty()) {
-        stringOutputs.push_back (MakeThaiOutput (isJson));
-        stringOutputs.push_back (MakeRomanOutput (isJson));
-        stringOutputs.push_back (MakePhoneticOutput (isJson));
+        stringOutputs.push_back (MakeThaiOutput (isJson, isGroup));
+        stringOutputs.push_back (MakeRomanOutput (isJson, isGroup));
+        stringOutputs.push_back (MakePhoneticOutput (isJson, isGroup));
     }
 
     if (1 == argc - optCnt) {
